89f0a776aa4e154161d4040ce3ff561fd828b2bd
wew571 <huy1872006k@gmail.com>
2025-10-18 22:32:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,21 @@
 # t : run BFS algorithm
 # y : run DFS algorithm
 # u : run Best First Search algorithm
+# i : run Dijkstra algorithm
+# m , n , b : generation maze
+# esc : quit
 # left button : make start point , finish point and make wall
 # right button : remove start point , finish point and wall
 
 import pygame
 from maze import main
 
-WIDTH = 736
-HEIGHT = 736
+WIDTH = 1000
+HEIGHT = 1000
+info_width = 300
 
-win = pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH + info_width , HEIGHT))
 pygame.display.set_caption("Maze Solver ")
 
-main(win, WIDTH)
+
+main(win , WIDTH)
