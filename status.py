ff80445d87f1d30036470d0caedaae3147aeab83
wew57 <huy1872006k@gmail.com>
2025-10-20 23:54:27 +0700

    Update status.py

--- status.py
+++ status.py
@@ -12,10 +12,10 @@ def draw_info_window(win, width, stats , speed):
     
     # Title
     font_title = pygame.font.SysFont('Arial', 28, bold=True)
-    title = font_title.render("SEARCH STATISTICS", True, color.UI_Title)
-    info_surface.blit(title, (20, 20))
+    title = font_title.render("Search Statistics", True, color.UI_Title)
+    info_surface.blit(title, (75, 20))
     
-    font = pygame.font.SysFont('Arial', 18)
+    font = pygame.font.SysFont('Arial', 20)
      
     y_offset = 60
     line_height = 30
@@ -23,7 +23,7 @@ def draw_info_window(win, width, stats , speed):
      # Status
     info_items = [
         ("Algorithm:", stats.get('algorithm', 'None')),
-        ("Time:", f"{stats.get('time', 0):.3f}s"),
+        ("Time:", f"{stats.get('time', 0):.5f}s"),
         ("Nodes Opened:", str(stats.get('nodes_opened', 0))),
         ("Nodes Closed:", str(stats.get('nodes_closed', 0))),
         ("Path Length:", str(stats.get('path_length', 0))),
@@ -38,6 +38,7 @@ def draw_info_window(win, width, stats , speed):
         info_surface.blit(value_text, (150, y_offset))
         y_offset += line_height
     
+    # Guide
     y_offset += 25
     instructions = [
         "Esc -- Quit ",
@@ -45,28 +46,31 @@ def draw_info_window(win, width, stats , speed):
         "Right Click -- Remove Start/End/Wall",
         "M -- Generate Maze",
         "Q -- Clear Path",
-        "E -- Reset Grid"
+        "E -- Reset Grid",
+        "W --- Increase Speed",
+        "S --- Decrease Speed"
     ]
     
-    mid_title = pygame.font.SysFont('Arial', 30, bold=True)
-    title = mid_title.render("GUIDE", True, color.UI_Title)
-    info_surface.blit(title, (40, y_offset))
+    guide_title = pygame.font.SysFont('Arial', 30, bold=True)
+    title = guide_title.render("Guide", True, color.UI_Title)
+    info_surface.blit(title, (75, y_offset))
 
     y_offset += 35
     
-    for instruction in instructions[0 :]:
+    for instruction in instructions:
         inst_text = font.render(instruction, True, color.UI_Guide)
         info_surface.blit(inst_text, (30, y_offset))
         y_offset += 20
     
-    y_offset += 20
-    end_title = pygame.font.SysFont('Arial' , 30 , bold=True)
-    title = end_title.render("ALGORITHM" , True , color.UI_Title)
-    info_surface.blit(title , (40 , y_offset))
+    # Solve Algorithm
+    y_offset += 30
+    solve_algorithm_title = pygame.font.SysFont('Arial' , 30 , bold=True)
+    title = solve_algorithm_title.render("Solve Algorithm" , True , color.UI_Title)
+    info_surface.blit(title , (75 , y_offset))
 
 
     y_offset += 35
-    algorithm_items = [
+    solve_algorithm_items = [
         "R --- A* Search",
         "T --- Breath First Search",
         "Y --- Depth First Search", 
@@ -74,9 +78,51 @@ def draw_info_window(win, width, stats , speed):
         "I --- Dijkstra",
     ]
 
-    for algorithm_item in algorithm_items:
+    for algorithm_item in solve_algorithm_items:
         algorithm_text = font.render(algorithm_item , True , color.UI_Text)
         info_surface.blit(algorithm_text , (30 , y_offset))
         y_offset += 20
+
+    # Build Maze
+    y_offset += 20
+    build_maze = pygame.font.SysFont('Arial' , 30 , bold=True)
+    title = build_maze.render("Build Maze" , True , color.UI_Title)
+    info_surface.blit(title , (75 , y_offset))
+
+    build_maze_algorithm_items = [
+        "M       DFS Algorithm",
+        "N       Prims Algorithm",
+        "B       Kruskals Algorithm"
+    ]
+
+    y_offset += 35
+    for buid_maze_algorithm_item in build_maze_algorithm_items:
+        build_maze_text = font.render( buid_maze_algorithm_item , True , color.UI_Text)
+        info_surface.blit(build_maze_text , (30 , y_offset))
+        y_offset += 20
+
+    # Difficult Maze
+    y_offset += 30
+    difficult_title = pygame.font.SysFont('Arial' , 30 , True)
+    title = difficult_title.render("Difficult Maze" , True , color.UI_Title)
+    info_surface.blit(title , (75 , y_offset)) 
+
+    y_offset += 35
+    label_part = font.render("Difficult:", True, color.UI_Text)
+    info_surface.blit(label_part, (30, y_offset))
+    difficult_value = stats.get('difficult', 'None')
+
+    if difficult_value == 'Easy':
+            color_value = color.UI_Difficult_Easy
+    elif difficult_value == 'Medium':
+        color_value = color.UI_Difficult_Medium
+    elif difficult_value == "Hard":
+        color_value = color.UI_Difficult_Hard
+    else:
+        color_value = color.UI_Difficult_None
+
+    value_part = font.render(difficult_value, True, color_value)
+    value_x = 30 + label_part.get_width() + 10  
+    info_surface.blit(value_part, (value_x, y_offset))
 
     win.blit(info_surface, (width, 0))
