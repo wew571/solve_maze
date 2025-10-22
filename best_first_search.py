359a5185e8960ba2c057ffba9c8751b3c1298cae
wew57 <huy1872006k@gmail.com>
2025-10-22 22:49:59 +0700

    Update best_first_search.py

--- best_first_search.py
+++ best_first_search.py
@@ -27,9 +27,15 @@ def solve(draw , grid , start , end , counter_start , speed):
 
     nodes_open = 0
     nodes_close = 0
+    total_nodes = 0
 
     draw_count = 0 
     total_draw_time_algorithm = 0
+
+    for row in grid:
+        for spot in row:
+            if not spot.is_wall() and not spot.is_outer_wall():
+                total_nodes += 1
 
     while not queue.empty():
         for e in pygame.event.get():
@@ -41,15 +47,17 @@ def solve(draw , grid , start , end , counter_start , speed):
         nodes_close += 1
 
         if  current == end:
+            exploration_ratio = nodes_close / total_nodes * 100
             path_data = time_solve.reconstruct_path(node_path , end , draw , counter_start , speed , total_draw_time_algorithm)
             start.make_start()
             end.make_end()
             return {
                 'time': path_data['time'],
-                'nodes_opened': nodes_open,
-                'nodes_closed': nodes_close,
-                'path_length': path_data['path_length'],
-                'path_found': True
+                'nodes_opened' : nodes_open,
+                'nodes_closed' : nodes_close,
+                'exploration_ratio' : exploration_ratio,
+                'path_length' : path_data['path_length'],
+                'path_found' : True
             }
         
         for  neibour in current.neighbours:
@@ -75,12 +83,16 @@ def solve(draw , grid , start , end , counter_start , speed):
         if current != start:
             current.make_close_bestfs()
 
+    exploration_ratio = nodes_close / total_nodes * 100
+    pygame.display.set_caption("Maze Solver ( Unable To Find The Target Node ! )")
+
     return {
         'time': default_timer() - counter_start,
-        'nodes_opened': nodes_open,
-        'nodes_closed': nodes_close,
-        'path_length': 0,
-        'path_found': False
+        'nodes_opened' : nodes_open,
+        'nodes_closed' : nodes_close,
+        'exploration_ratio' : exploration_ratio,
+        'path_length' : 0,
+        'path_found' : False
     }
 
 
