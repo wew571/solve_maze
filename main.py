a8707dd066b76dcc0272419065ddafa82aa5e078
wew57 <huy1872006k@gmail.com>
2025-10-22 22:44:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 # u : run Best First Search algorithm
 # i : run Dijkstra algorithm
 # m , n , b : generation maze
+# g : delete history , best results
+# w : increase speed search
+# s : decrease speed search
 # esc : quit
 # left button : make start point , finish point and make wall
 # right button : remove start point , finish point and wall
@@ -16,9 +19,11 @@ from maze import main
 
 WIDTH = 1000
 HEIGHT = 1000
-info_width = 300
 
-win = pygame.display.set_mode((WIDTH + info_width , HEIGHT))
+info_width = 300
+sub_width = 600
+
+win = pygame.display.set_mode((WIDTH + info_width + sub_width , HEIGHT))
 pygame.display.set_caption("Maze Solver ")
 
 
